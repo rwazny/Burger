ec1e9deeb3243929657c4b76d0ef735dfdd82676
Mr Scary <ronwazny@gmail.com>
2019-02-07 17:58:03 -0500

    initial try

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Basic Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Bacon Bomb Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Triple Stack Heart Attack Burger');
+
